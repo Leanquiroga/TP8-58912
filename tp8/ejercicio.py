c6a0b818da930e2e80db26d923abb727fb3c6add
Leanquiroga <143456057+Leanquiroga@users.noreply.github.com>
2024-11-21 18:33:30 -0300

    Update ejercicio.py

--- tp8/ejercicio.py
+++ tp8/ejercicio.py
@@ -4,13 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ## ATENCION: Debe colocar la direccion en la que ha sido publicada la aplicacion en la siguiente linea\
-# url = 'import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-## ATENCION: Debe colocar la direccion en la que ha sido publicada la aplicacion en la siguiente linea\
-url = 'https://tp8-555555.streamlit.app/'
+url = 'https://tp8-58912.streamlit.app/'
 
 def mostrar_datos_usuario():
     with st.container(border=True):
